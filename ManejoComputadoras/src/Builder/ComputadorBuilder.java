a869dfb8f33405e2dafb82a1c9a0b69e49e34297
FerSaul <fsmoreno@espol.edu.ec>
2018-12-11 13:09:03 -0500

    Modificacion

--- ManejoComputadoras/src/Builder/ComputadorBuilder.java
+++ ManejoComputadoras/src/Builder/ComputadorBuilder.java
@@ -11,10 +11,7 @@ public abstract class ComputadorBuilder {
     public Computador getComputador() {
         return c;
     }
-    
-    
- 
-
+   
     
 
     public abstract void setRam();
@@ -34,51 +31,6 @@ public abstract class ComputadorBuilder {
     public abstract void setCoolerExterno();
 
 
-
-
-
-
-	public void setRAM() {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-
-
-
-
-	public void SistemaOperativo() {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-
-
-
-
-	public void setMainboard() {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-
-
-
-
-	public void setComputador() {
-		// TODO Auto-generated method stub
-		
-	}
-    
-    
-
-      
-    
-    
-    
     
     
     
